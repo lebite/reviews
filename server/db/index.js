5468c54c0df35e824dfad1a8eef0b1a02209b92e
Julio Fuentes <juliofuentes408@gmail.com>
2019-07-24 10:39:17 -0700

    Refactored database index.js file to point to localhost

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: '172.17.0.2',
-  // host: 'localhost',
+  // host: '172.17.0.2',
+  host: 'localhost',
   user: 'root',
-  database: 'reservlyreviews',
-  password: 'password'
+  database: 'reservlyreviews'
+  // password: 'password'
 });
 
 connection.connect();
